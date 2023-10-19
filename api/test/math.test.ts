73a8f69b79f4023760938d4eb4f0f077539aaffe
Pablo Contreras <pacg1991@gmail.com>
2023-10-19 07:35:19 -0500

    nosql basic added

--- api/test/math.test.ts
+++ api/test/math.test.ts
@@ -5,6 +5,7 @@ import { describe, expect, it, afterAll } from "bun:test";
 const baseUrl = `http://cv_test_api:8000`;
 
 describe("GET Users suite", () => {
+  console.log("baseUrl", baseUrl);
   it("should return a list of users successfully", async () => {
     const req = new Request(baseUrl);
     const res = await fetch(req);
